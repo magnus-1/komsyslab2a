88f2ae1b62e5112b16882c1bc524e6417908609c
sirprice <carljohan.dahlman@gmail.com>
2016-10-11 11:52:31 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,43 @@
 /**
  * Created by cj on 16/09/16.
  */
+
 import clientapplication.User;
 import serverapplication.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.util.Scanner;
 
-public class  Main {
+public class Main {
 
     private static final String CLIENT = "client";
     private static final String SERVER = "server";
 
     public static void main(String[] args) throws IOException {
 
-        if  (args[0].toLowerCase().equals(CLIENT)){
+        if (args[0].toLowerCase().equals(CLIENT)) {
             System.out.println("Starting Client...");
-            //InetSocketAddress addr = new InetSocketAddress(args[1],Integer.parseInt(args[2]));
-            User user = new User(null);
-            user.start();
+            try {
+
+                String url = "rmi://" + args[1] + "/" + args[2];
+                ServerRMI server = (ServerRMI) Naming.lookup(url);
+                User user = new User(server);
+                server.registrateClient(user);
+                user.start();
+
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
+
         }
-        if (args[0].toLowerCase().equals(SERVER)){
+        if (args[0].toLowerCase().equals(SERVER)) {
             System.out.println("Starting Server...");
             Server server = new Server();
-            //server.start();
+            Naming.rebind(args[1], server);
+
         }
     }
 }
